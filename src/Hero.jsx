26672e7476e2f3066de2c0daa6efb7463474eb34
Muneesa Majid <muneesamajid9@gmail.com>
2024-05-22 12:36:04 +0530

    Responsive design hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center space-y-8 md:space-y-0 md:space-x-8 mt-14 px-4 md:px-16 lg:px-16 sm:px-16">
       <div className=" md:text-left space-y-4  lg:w-2/5 xl:w-1/3">
-        <h4 className="text-[#0066FC] font-bold  md:text-3xl font-poppins ">
+        <h4 className="text-[#0066FC] font-bold text-xl md:text-2xl font-poppins ">
           <strong>Content Syndication</strong>
         </h4>
         <h1 className="text-5xl font-bold text-[#05195F] leading-snug md:leading-relaxed">
