bc07c95061372812abc4924b1e5f2a0bbd956138
iqra <iqra.magray@enablenow.com>
2024-05-22 12:16:13 +0530

    made changes in these files

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 
 import TrendingBlogs from './CarouselComponent.jsx'
 import Card from './card.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    
-  <Card />
-    <TrendingBlogs />
+    <App /> 
   </React.StrictMode>,
 )
